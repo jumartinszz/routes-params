462a7af6da99758cbfefe424b4f8dbf871e84411
jumartinss <123407157+jumartinszz@users.noreply.github.com>
2024-03-13 09:36:39 -0300

    Initial commit

---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import Title from '../../components/Title';
+import styles from './styles';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Title title={"Home - Hello today birthday felipe dev"}/>
+    </View>
+  )
+}
